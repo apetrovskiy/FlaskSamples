e22ef14ab6f8f07e3e4cf87b537d6e62642cbbd3
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-01-17 16:50:02 +0300

    v.013

--- DwyerAggarwalStouffer/headlines.py
+++ DwyerAggarwalStouffer/headlines.py
@@ -12,11 +12,15 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 DEFAULTS = {'publication': 'bbc',
-            'city': 'London,UK'}
+            'city': 'London,UK',
+            'currency_from': 'GBP',
+            'currency_to': 'USD'}
 
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/" \
               "weather?q={}&units=metric&appid=" \
               "07147b0d16f3958f4584aff355c4ad82"
+CURRENCY_URL = "https://openexchangerates.org/api/latest.json?" \
+    "app_id=61a10f2443de4ccea6fabcedfd9fadc3"
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,8 +35,19 @@ def home():
     if not city:
         city = DEFAULTS['city']
     weather = get_weather(city)
+    # currency
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate = get_rate(currency_from, currency_to)
     return render_template("home.html",
-                           articles=articles, weather=weather)
+                           articles=articles, weather=weather,
+                           currency_from=currency_from,
+                           currency_to=currency_to,
+                           rate=rate)
 
 
 def get_news(query):
@@ -54,8 +69,17 @@ def get_weather(query):
         weather = {"description":
                    parsed["weather"][0]["description"],
                    "temperature": parsed["main"]["temp"],
-                   "city": parsed["name"]}
+                   "city": parsed["name"],
+                   'country': parsed['sys']['country']}
     return weather
+
+
+def get_rate(frm, to):
+    all_currency = urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate/frm_rate
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
