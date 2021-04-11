e24c99864de1a97d6278e80e00326f55ee3fc9a3
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-12 00:08:57 +0300

    templates

--- flask_app/wsgi.py
+++ flask_app/wsgi.py
@@ -1,16 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def get_films():
+    return [
+        {
+            'id': 1,
+            'title': 'Harry Potter and the Philosopher\'s Stone',
+            'release_date': 'November 4, 2001'
+        },
+        {
+            'id': 2,
+            'title': 'Harry Potter and Chamber of Secrets',
+            'release_date': 'November 3, 2002'
+        }
+    ]
+
+
 @app.route('/')
+@app.route('/hello')
 def index():
-    return '<h1>Hello, World!</h1>'
+    films = get_films()
+    return render_template('hello.html', films=films)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About</h1>'
+    return render_template('about.html')
+
+
+@app.route('/<string:name>')
+def greeting(name: str):
+    return f'Hello, {name.capitalize()}'
 
 
 if __name__ == '__main__':
